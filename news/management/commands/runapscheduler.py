ad3894bf07f42099c53ba03652e7f62dfb12fb7c
Marleono <73365729+Marleono@users.noreply.github.com>
2021-05-12 18:14:43 +0300

    Add files via upload

---
+++ news/management/commands/runapscheduler.py
@@ -0,0 +1,27 @@
+import logging
+
+from django.conf import settings
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+
+logger = logging.getLogger(__name__)
+
+
+def my_job():
+    #  Your job processing logic here...
+    print('hello from job')
+
+
+# функция которая будет удалять неактуальные задачи
+def delete_old_job_executions(max_age=604_800):
+    """This job deletes all apscheduler job executions older than `max_age` from the database."""
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
+class Command(BaseCommand):
+    help = "Runs apscheduler."
+
